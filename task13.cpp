c6739145d36f5809215a7227945fae74fcbe9d35
AfifMasood 180 <144409057+AfifMasood@users.noreply.github.com>
2023-09-15 19:49:33 +0500

    Add files via upload

---
+++ task13.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std;
+main()
+{  
+ cout<<"      +--^----------,--------,-----,--------^--, "<<endl;
+ cout<<"       | |||||||||   '--------'     |           O"<<endl;
+ cout<<"       '+---------------------------^-----------| "<<endl;
+ cout<<"         ',---------,---------,---------------,"<<endl;
+ cout<<"           / XXXXXX /'|       /' "<<endl;
+ cout<<"          / XXXXXX /  |      /'"<<endl;
+ cout<<"         / XXXXXX /'-------'"<<endl;
+ cout<<"        / XXXXXX /"<<endl;
+ cout<<"       / XXXXXX /"<<endl;
+ cout<<"      (________(   "<<endl;
+ cout<<"       '______' ";
+}
+            
+ 
